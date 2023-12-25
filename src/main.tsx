d4b477f5b657f5a4eb1098b02518eebef9fac88a
Joel Almeida <joel.almeida.sa@gmail.com>
2023-12-25 18:27:54 -0300

    Add Redux store provider to main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -4,13 +4,17 @@ import { App } from "./App.tsx";
 import { GlobalStyle } from "./styles/global-styles/index.tsx";
 import { ThemeProvider } from "styled-components";
 import { defaultTheme } from "./styles/theme/defaultTheme.tsx";
+import { Provider } from "react-redux";
+import { store } from "././redux/store";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <GlobalStyle />
+    <Provider store={store}>
+      <GlobalStyle />
 
-    <ThemeProvider theme={defaultTheme}>
-      <App />
-    </ThemeProvider>
+      <ThemeProvider theme={defaultTheme}>
+        <App />
+      </ThemeProvider>
+    </Provider>
   </React.StrictMode>,
 );
