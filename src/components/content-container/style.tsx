386d6b33a45b947cf8991ec34f866bc0e4772f0f
Joel Almeida <joel.almeida.sa@gmail.com>
2023-12-25 09:43:33 -0300

    Add ContentContainer component style

---
+++ src/components/content-container/style.tsx
@@ -0,0 +1,9 @@
+import style from "styled-components";
+
+const ContentContainer = style.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+`;
+
+export { ContentContainer };
