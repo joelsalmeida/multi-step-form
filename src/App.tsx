603c1d326a6edefa00c89e4ecf3949b337082198
Joel Almeida <joel.almeida.sa@gmail.com>
2023-12-31 16:35:53 -0300

    Add Redux functionality and update step navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
 import style from "styled-components";
+import { useDispatch, useSelector } from "react-redux";
+
 import { Button } from "./components/button";
 import { Step } from "./components/step";
 import { StepContainerStyled } from "./components/step-container/style";
@@ -6,6 +8,9 @@ import { FormContainerStyled } from "./components/form-container/styled";
 import { NavigationContainerStyled } from "./components/navigation-container/style";
 import { ContentContainer } from "./components/content-container/style";
 import { PersonalInfoForm } from "./components/form-steps/PersonalInfoForm";
+import { PlanForm } from "./components/form-steps/PlanForm";
+import { RootStateType } from "./redux/store";
+import { setCurrentStep } from "./redux/slices/steps";
 
 const Base = style.div`
   inset: 0;
@@ -13,22 +18,43 @@ const Base = style.div`
 `;
 
 function App() {
+  const steps = [<PersonalInfoForm />, <PlanForm />];
+  const stepIds = ["personalInfo", "plan"];
+
+  const currentStep = useSelector(
+    (state: RootStateType) => state.steps.currentStep,
+  );
+
+  const dispatch = useDispatch();
+  const goBack = () => {
+    dispatch(setCurrentStep({ currentStep: currentStep - 1 }));
+  };
+
   return (
     <Base>
       <FormContainerStyled>
         <StepContainerStyled>
-          <Step number={1} description="Inactive Step" />
-          <Step number={2} description="Active Step" active />
-          <Step number={3} description="Inactive Step" />
-          <Step number={4} description="Inactive Step" />
+          <Step number={1} description="Your info" active={currentStep === 0} />
+          <Step
+            number={2}
+            description="Select plan"
+            active={currentStep === 1}
+          />
+          <Step number={3} description="Add-ons" active={currentStep === 2} />
+          <Step number={4} description="Summary" active={currentStep === 3} />
         </StepContainerStyled>
 
         <ContentContainer>
-          <PersonalInfoForm />
+          {steps[currentStep]}
 
           <NavigationContainerStyled>
-            <Button disabled>Go Back</Button>
-            <Button>Next Step</Button>
+            <Button disabled={currentStep === 0} onClick={goBack}>
+              Go Back
+            </Button>
+
+            <Button type="submit" form={stepIds[currentStep]}>
+              Next Step
+            </Button>
           </NavigationContainerStyled>
         </ContentContainer>
       </FormContainerStyled>
