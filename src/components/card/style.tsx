824d7d25fa2844e14e7bc66e252e91e62bad7c5a
Joel Almeida <joel.almeida.sa@gmail.com>
2024-01-01 16:57:12 -0300

    Add responsive styles to Card component

--- src/components/card/style.tsx
+++ src/components/card/style.tsx
@@ -1,6 +1,7 @@
 import style from "styled-components";
 
 const CardStyled = style.label<{ $active?: boolean }>`
+  min-height: 6rem;
   display: flex;
   background-color: ${(props) => props.theme.neutral300};
   border: 1px solid ${(props) =>
@@ -8,6 +9,15 @@ const CardStyled = style.label<{ $active?: boolean }>`
   border-radius: 4px;
   padding: 1rem;
   margin-bottom: 1rem;
+
+  @media (min-width: 768px) {
+    min-width: 8.5rem;
+    min-height: 11.5rem;
+    flex-direction: column;
+    justify-content: space-between;
+    align-items: flex-start;
+    gap: 3rem;
+  }
 `;
 
 export { CardStyled };
