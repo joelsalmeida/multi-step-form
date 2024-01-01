7624289cd2c513dc4b375ed06e1cc9018d8d7cee
Joel Almeida <joel.almeida.sa@gmail.com>
2024-01-01 16:57:41 -0300

    Update margin-left in PlanContentContainerStyled

--- src/components/plan-card/style.tsx
+++ src/components/plan-card/style.tsx
@@ -5,6 +5,10 @@ const PlanContentContainerStyled = style.div`
   flex-direction: column;
   line-height: 1.2rem;
   margin-left: 1rem;
+
+  @media (min-width: 768px) {
+    margin-left: 0;
+  }
 `;
 
 export { PlanContentContainerStyled };
