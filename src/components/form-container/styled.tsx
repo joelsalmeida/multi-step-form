fe2151456569927f8beb3b2bdcf7401eb2976a10
Joel Almeida <joel.almeida.sa@gmail.com>
2024-01-02 12:16:07 -0300

    Update padding in FormContainerStyled

--- src/components/form-container/styled.tsx
+++ src/components/form-container/styled.tsx
@@ -2,13 +2,14 @@ import style from "styled-components";
 
 const FormContainerStyled = style.div`
   margin: 1rem auto;
-  padding: 2rem;
+  padding: 1.5rem;
   border-radius: 8px;
   background-color: ${(props) => props.theme.neutral200};
   box-shadow: 0px 4px 32px 0px rgba(150,153,171,0.5);
   margin-top: 80px;
-
+  
   @media (min-width: 768px) {
+    padding: 2rem;
     display: flex;
     max-width: 940px;
     margin-top: 1rem;
